b9d53bcb228c3575af901cb54d1e023a0dc31389
Pablo <pablomrmr@gmail.com>
2022-05-26 19:47:54 +0200

    Mejora el diseño responsive de las pantallas de contacto y nosotros

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -6,22 +6,21 @@
     <hr/>
 
     <div class="row pt-4">
-        <div class="col d-flex">
-            <div class="card" style="width: 65%;">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11748.298554487048!2d-5.5763941!3d42.5961599!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd522320326519e4d!2sCD%20PLAY!5e0!3m2!1ses!2ses!4v1652297383410!5m2!1ses!2ses" width="950" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-            </div>
-            <div class="mx-4 card" style="width: 30%;">
-                <div class="card-body">
-                    <h5 class="card-title">CDShop</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Formas de contacto</h6>
-                    <p class="card-text pt-3"><span class="material-icons text-black">place</span><strong> Dirección</strong>: Calle Carmen, 8-10 24001 León</p>
-                    <p class="card-text pt-1"><span class="material-icons text-black">mail</span><strong> Correo</strong>: cdShop@gmail.com</p>
-                    <p class="card-text pt-1"><span class="material-icons text-black">call</span><strong> Telefono</strong>: 987 45 63 21</p>
-                    <p class="card-text pt-1"><span class="material-icons text-black">phone_iphone</span><strong> Móvil</strong>: 687 45 98 99</p>
-                    <h6 class="card-subtitle mt-5 mb-2 text-muted">Redes Sociales</h6>
-                    <p class="card-text pt-3"><strong> Instagram</strong>: cdShopLeon</p>
-                    <p class="card-text pt-1"><strong> facebook</strong>: cdShop1975</p>
-                </div>
+        <div class="col-12 col-sm-9 col-lg-8 d-flex align-items-center">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11748.298554487048!2d-5.5763941!3d42.5961599!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd522320326519e4d!2sCD%20PLAY!5e0!3m2!1ses!2ses!4v1652297383410!5m2!1ses!2ses" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+        
+        <div class="col-12 col-sm-7 col-lg-4 d-flex align-items-center">
+            <div class="card-body">
+                <h5 class="card-title">CDShop</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Formas de contacto</h6>
+                <p class="card-text pt-3"><span class="material-icons text-black">place</span><strong> Dirección</strong>: Calle Carmen, 8-10 24001 León</p>
+                <p class="card-text pt-1"><span class="material-icons text-black">mail</span><strong> Correo</strong>: cdShop@gmail.com</p>
+                <p class="card-text pt-1"><span class="material-icons text-black">call</span><strong> Telefono</strong>: 987 45 63 21</p>
+                <p class="card-text pt-1"><span class="material-icons text-black">phone_iphone</span><strong> Móvil</strong>: 687 45 98 99</p>
+                <h6 class="card-subtitle mt-5 mb-2 text-muted">Redes Sociales</h6>
+                <p class="card-text pt-3"><strong> Instagram</strong>: cdShopLeon</p>
+                <p class="card-text pt-1"><strong> facebook</strong>: cdShop1975</p>
             </div>
         </div>
     </div>
