d8041d1c9a07a4163ee8e5fb2684dcccb0c96664
Pablo <pablomrmr@gmail.com>
2022-05-30 21:44:31 +0200

    Modifica la migracion de users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('last_name');
+            $table->string('address');
+            $table->string('type');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
